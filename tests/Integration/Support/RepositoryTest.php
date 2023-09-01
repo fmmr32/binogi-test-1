be5396bac8368c6df5830eed7854fad363d10d84
MushfiqurSun <fmmr32@mun.ca>
2023-09-01 19:47:15 -0300

    update repository test
    test to see if it fails to create a user if we provide insufficient/ required data

--- tests/Integration/Support/RepositoryTest.php
+++ tests/Integration/Support/RepositoryTest.php
@@ -22,37 +22,74 @@ class RepositoryTest extends FrameworkTest
         $this->assertEquals(User::get(), $this->repository->all());
     }
 
-    // public function testCreate()
-    // {
-    //     $user = $this->userFactory->create();
-    //     $this->assertInstanceOf(User::class, $user);
-    // }
     public function testCreate()
     {
+        $user = $this->userFactory->create();
+        $this->assertInstanceOf(User::class, $user);
+    }
+    /** a hard coded way to test create user */
+    // public function testCreate()
+    // {
+    //     $userData = [
+    //         'name' => 'James Joe',
+    //         'email' => 'james@example.com',
+    //         'password' => bcrypt('password'),
+    //         'nickname' => 'james', // Include nickname
+    //     ];
+
+    //     $user = $this->repository->create($userData);
+
+    //     $this->assertInstanceOf(User::class, $user);
+    //     $this->assertEquals($userData['name'], $user->name);
+    //     $this->assertEquals($userData['email'], $user->email);
+    //     $this->assertEquals($userData['nickname'], $user->nickname); // Ensure 'nickname' is correctly stored
+    // }
+
+    // public function testCreateWithoutSuffientData()
+    // {
+    //     $userData = [
+    //         'name' => '',
+    //         'email' => '',
+    //         'password' => bcrypt('password'),
+    //         'nickname' => '', // Include nickname
+    //     ];
+
+    //     $user = $this->repository->create($userData);
+
+    //     $this->assertInstanceOf(User::class, $user);
+    //     $this->assertEquals($userData['name'], $user->name);
+    //     $this->assertEquals($userData['email'], $user->email);
+    //     $this->assertEquals($userData['nickname'], $user->nickname); // Ensure 'nickname' is correctly stored
+    // }
+
+    public function testCreateFailsWithoutSufficientData()
+    {
+        //  incomplete user data
         $userData = [
-            'name' => 'James Joe',
-            'email' => 'james@example.com',
+            'name' => '',
+            // Missing name
+
+            'email' => 'test@example.com',
             'password' => bcrypt('password'),
-            'nickname' => 'james', // Include nickname
+            'nickname' => '', // Missing nickname
         ];
 
+        // Attempt to create a user with incomplete data
         $user = $this->repository->create($userData);
 
-        $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals($userData['name'], $user->name);
-        $this->assertEquals($userData['email'], $user->email);
-        $this->assertEquals($userData['nickname'], $user->nickname); // Ensure 'nickname' is correctly stored
+        // Expecting a validation error
+        $this->assertNull($user);
+
+        // Ensure that there are validation errors
+        $this->assertTrue($this->repository->hasErrors());
+
+        // Validate that the response includes validation error messages for the missing fields
+        $this->assertArrayHasKey('name', $this->repository->getErrors());
+        $this->assertArrayHasKey('nickname', $this->repository->getErrors());
+
+        // Ensure that no user is created in the database
+        $this->assertEquals(0, User::count());
     }
-    // public function testUpdate()
-    // {
-    //     $user = $this->userFactory->create();
-    //     $this->assertInstanceOf(User::class, $user);
-    //     $this->repository->update(['name' => 'Luke Skywalker'], $user->id);
-    //     // update nickname
-    //     $this->repository->update(['nickname' => 'Lukey'], $user->id);
-    //     $this->assertEquals('Luke Skywalker', $user->refresh()->name);
-    //     $this->assertEquals('Lukey', $user->refresh()->nickname);
-    // }
 
     public function testUpdate()
     {
@@ -79,23 +116,14 @@ class RepositoryTest extends FrameworkTest
         $this->repository->delete($user->id);
         $this->assertFalse(User::where('id', $user->id)->exists());
     }
-
-    // public function testFind()
-    // {
-    //     $user = $this->userFactory->create();
-    //     $found = $this->repository->find($user->id);
-    //     $this->assertInstanceOf(User::class, $found);
-    //     $this->assertEquals($user->id, $found->id);
-    // }
     public function testFind()
     {
         $user = $this->userFactory->create();
+        $found = $this->repository->find($user->id);
 
-        $foundUser = $this->repository->find($user->id);
-
-        $this->assertInstanceOf(User::class, $foundUser);
-        $this->assertEquals($user->id, $foundUser->id);
-        $this->assertEquals($user->nickname, $foundUser->nickname); // Ensure 'nickname' is retrieved correctly
+        $this->assertInstanceOf(User::class, $found);
+        $this->assertEquals($user->id, $found->id);
+        $this->assertEquals($user->nickname, $found->nickname); // Ensure 'nickname' is retrieved correctly
     }
 
 }
